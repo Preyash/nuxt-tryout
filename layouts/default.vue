848393c260f5eaf7586b42278f94e36430fd272c
preyash <phojiwala.techark@gmail.com>
2024-04-17 16:56:23 +0530

    minor changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,10 @@
 <script setup>
-
 </script>
 
 <template>
   <CustomNavbar />
-  <div class="mx-6">
+  <div class="mx-6 mt-[40px]">
     <slot></slot>
   </div>
   <Toaster duration="1500" />
 </template>
-

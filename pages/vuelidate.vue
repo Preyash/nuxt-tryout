f2e453ceb657a837777505f59e0748e73d8d0264
preyash <phojiwala.techark@gmail.com>
2024-04-29 16:20:56 +0530

    test page removed

--- pages/vuelidate.vue
+++ pages/vuelidate.vue
@@ -1,11 +1,3 @@
-<script setup>
-useHead({
-  title: "Vuelidate Page",
-  meta: [{ name: "description", content: "Utilizing Vuelidate for form validation" }],
-  link: [{ rel: "stylesheet", href: "https://unpkg.com/chota@latest" }],
-});
-</script>
-
 <template>
   <Vuelidate />
 </template>
